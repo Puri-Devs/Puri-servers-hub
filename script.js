8af9cff23aa3a91f14d14fa861dc98017f0fe016
NoNameUwU <91313779+NoNameUwU@users.noreply.github.com>
2022-06-29 02:55:38 +0200

    new

---
+++ script.js
@@ -0,0 +1,11 @@
+function eheh() {
+    window.open("https://eheh.online")
+}
+function mirror() {
+    window.open("https://cosmos-mirror.eu")
+}
+
+
+function open_window(url){
+    window.open(url)
+}
